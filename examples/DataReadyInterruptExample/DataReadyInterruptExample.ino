309e56c6e236e046cdaa4060a01b9f56b5e2b941
leoli51 <leo_la_rocca@yahoo.it>
2021-09-28 14:38:05 +0200

    updated interrupt example

--- examples/DataReadyInterruptExample/DataReadyInterruptExample.ino
+++ examples/DataReadyInterruptExample/DataReadyInterruptExample.ino
@@ -4,7 +4,7 @@ Melopero_VL53L1X sensor;
 
 // Interrupt variables and callback
 bool interruptOccurred = false;
-byte intPin = 1;
+byte intPin = 24;
 
 void interruptCallback(){
     interruptOccurred = true;
@@ -17,21 +17,24 @@ void setup() {
   Serial.println("Starting...");
 
   VL53L1_Error status = 0;
-  status = sensor.initDevice();
-  printStatus("Device initialized : ", sensor.errorString);
+  Wire.begin(); // use Wire1.begin() to use I2C-1 
+  sensor.initI2C(0x29, Wire); // use sensor.initI2C(0x29, Wire1); to use I2C-1
+  
+  status = sensor.initSensor();
+  printStatus("Device initialized : ", status);
   
   status = sensor.setDistanceMode(VL53L1_DISTANCEMODE_MEDIUM);
-  printStatus("Set distance mode : ", sensor.errorString);
+  printStatus("Set distance mode : ", status);
 
   /*  Timing budget is the time required by the sensor to perform one range 
    *  measurement. The minimum and maximum timing budgets are [20 ms, 1000 ms] */
   status = sensor.setMeasurementTimingBudgetMicroSeconds(66000);
-  printStatus("Set timing budget: ", sensor.errorString);
+  printStatus("Set timing budget: ", status);
 
   /*  Sets the inter-measurement period (the delay between two ranging operations) in milliseconds. The minimum 
    *  inter-measurement period must be longer than the timing budget + 4 ms.*/
   status = sensor.setInterMeasurementPeriodMilliSeconds(75);
-  printStatus("Set inter measurement time: ", sensor.errorString);
+  printStatus("Set inter measurement time: ", status);
 
   //If the above constraints are not respected the status is -4: VL53L1_ERROR_INVALID_PARAMS
   
@@ -39,8 +42,10 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(intPin), interruptCallback, CHANGE);
   
   // Enable the data ready interrupts
-  sensor.enableInterruptOnDataReady();
-  sensor.startMeasurement();
+  status = sensor.enableInterruptOnDataReady();
+  printStatus("Enable interrupts: ", status);
+  status = sensor.clearInterruptAndStartMeasurement();
+  printStatus("Start measurement: ", status);
 }
 
 void loop() {
@@ -48,17 +53,17 @@ void loop() {
   
   if (interruptOccurred){
     status = sensor.getRangingMeasurementData();
-    if (status != VL53L1_ERROR_NONE) printStatus("Error in get measurement data: ",  sensor.errorString);
+    if (status != VL53L1_ERROR_NONE) printStatus("Error in get measurement data: ",  status);
 
     status = sensor.clearInterruptAndStartMeasurement();
-    if (status != VL53L1_ERROR_NONE) printStatus("Error in clear interrupts: ",  sensor.errorString);
+    if (status != VL53L1_ERROR_NONE) printStatus("Error in clear interrupts: ",  status);
 
     Serial.print((float)sensor.measurementData.RangeMilliMeter + (float)sensor.measurementData.RangeFractionalPart/256.0);
     Serial.println(" mm");
   }
 }
 
-void printStatus(String msg, String status){
+void printStatus(String msg, VL53L1_Error status){
   Serial.print(msg);
-  Serial.println(sensor.errorString);
+  Serial.println(status);
 }
